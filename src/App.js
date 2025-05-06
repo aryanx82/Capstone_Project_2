8ed7d14621bbad54a02648383231f0f9f08daad0
Aryan Yadav <aryan.yadav2024@nst.rishihood.edu.in>
2025-05-06 11:27:15 +0530

    Starting commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/header';
+import Main from './components/main';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <div className='divider'/>
+      <Main/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/main.js
@@ -0,0 +1,55 @@
+import React,{useState,useEffect} from 'react'
+import Search from './search'
+
+export default function Main() {
+
+    const [content,setContent]=useState('Home')
+    const [data, setData] = useState(null);
+
+
+    useEffect(() => {
+        let url = '';
+    
+        if (content === 'Coursera') {
+          url = 'https://jsonplaceholder.typicode.com/posts/1';
+        } else if (content === 'Udemy') {
+          url = 'https://jsonplaceholder.typicode.com/posts/2';
+        } else if (content === 'Edx') {
+          url = 'https://jsonplaceholder.typicode.com/posts/3';
+        }
+
+        if (url) {
+          fetch(url)
+            .then((res) => res.json())
+            .then((data) => setData(data))
+            .catch((err) => console.error('Fetch error:', err));
+        } else {
+          setData(null);
+        }
+      }, [content]);
+    
+  return (
+    <>
+        <div className='main'>
+            <div className='buttons'>
+            <button className='box' onClick={() => setContent('Coursera')} >Coursera</button>
+            <div className='part' style={{width: 1.38, height: 85.99}}></div>
+            <button className='box' onClick={() => setContent('Udemy')}>Udemy</button>
+            <div className='part' style={{width: 1.38, height: 85.99}}></div>
+            <button className='box' onClick={() => setContent('Edx')}>EDX</button>
+            <div className='part' style={{width: 1.38, height: 85.99}}></div>
+            <button className='box' onClick={()=>setContent('Bookmark')}>Bookmark</button>
+            </div>
+            <Search/>
+        {data ? (
+          <div className="results">
+            <h3>Fetched Data:</h3>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+          </div>
+        ) : (
+          content !== 'Home' && <p>Loading or no data available.</p>
+        )}
+        </div>
+    </>
+  )
+}
